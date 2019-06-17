9635385f2ef69d1e73bdf22d5c00740277de790d
Mark Kretschmann <mark.kretschmann@asys-group.com>
2019-06-17 10:02:29 +0200

    more

--- SimplexOpcServer/SimplexOpcServer.cpp
+++ SimplexOpcServer/SimplexOpcServer.cpp
@@ -9,6 +9,12 @@ SimplexOpcServer::SimplexOpcServer(QWidget *parent)
 
     m_server = new QUaServer();
 
+    connect(ui.startServerButton, &QPushButton::clicked, this, &SimplexOpcServer::startServer);
+    connect(ui.stopServerButton, &QPushButton::clicked, this, &SimplexOpcServer::stopServer);
+}
+
+void SimplexOpcServer::startServer()
+{
     QUaFolderObject * objsFolder = m_server->objectsFolder();
 
     // register new type
@@ -17,13 +23,16 @@ SimplexOpcServer::SimplexOpcServer(QWidget *parent)
 
     // create new type instances
 
+    const auto numNestedNodes = ui.numNodes->value();
+    const auto numVariables = ui.numVariables->value();
+
     QUaBaseObject * sensor = nullptr;
 
-    for ( int si = 0; si < 1000; si++ ) {
+    for ( int si = 0; si < numNestedNodes; si++ ) {
         sensor = objsFolder->addChild<TemperatureSensor>();
         sensor->setDisplayName(QString("Sensor%1").arg(si));
 
-        for ( int i = 0; i < 10; i++ ) {
+        for ( int i = 0; i < numVariables; i++ ) {
             auto varBaseData = sensor->addBaseDataVariable(QString("ns=1;s=%1_StringVar%2").arg(sensor->displayName()).arg(i));
             varBaseData->setWriteAccess(true);
             varBaseData->setDataType(QMetaType::QString);
@@ -32,14 +41,16 @@ SimplexOpcServer::SimplexOpcServer(QWidget *parent)
         }
     }
 
-    connect(ui.startServerButton, &QPushButton::clicked, this, &SimplexOpcServer::startServer);
-}
-
-void SimplexOpcServer::startServer()
-{
     m_server->start();
+
+    ui.startServerButton->setEnabled(false);
+    ui.stopServerButton->setEnabled(true);
 }
 
 void SimplexOpcServer::stopServer()
 {
+    m_server->stop();
+
+    ui.startServerButton->setEnabled(true);
+    ui.stopServerButton->setEnabled(false);
 }
